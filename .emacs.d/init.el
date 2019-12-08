50b097e042240c0e72d99b03e4ed9e1ed52a6fa7
Kamil Nikel <kamil@fmap.io>
2019-12-08 11:14:34 +0100

    feat(shell): add .env_* files to split langs/context

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,7 @@
 (require 'package)
 
 (setq package-archives
-      '(
+      '(("gnu" . "https://elpa.gnu.org/packages/")
         ("melpa" . "http://melpa.org/packages/")
         ("melpa-stable" . "http://stable.melpa.org/packages/")
         ("org-elpa" . "https://orgmode.org/elpa/")))
